78aadfb1d40c6e2353fa46cee4bf9e0c64ca5df5
ahmaddhani14 <ahmadhaniathaillah@gmail.com>
2025-11-27 23:54:38 +0700

    Added font & other stuff

--- lib/main.dart
+++ lib/main.dart
@@ -2,42 +2,69 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+
 import 'package:intl/date_symbol_data_local.dart';
 
 import 'home_page.dart';
-import 'login_page.dart';
 import 'splash_screen.dart';
+import 'service/font_service.dart';
+
+String globalFontSize = "Medium";
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
   await initializeDateFormatting('id', null);
-
+  // Load font user saat aplikasi dibuka
+  globalFontSize = await FontService.getFontSize();
   runApp(const MyApp());
 }
+double getFontSize(String size) {
+  switch (size) {
+    case "Small":
+      return 12;
+    case "Large":
+      return 20;
+    default:
+      return 16;
+  }
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key}); // ⬅ super.key tetap dipakai
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Wallet Helper',
       theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme(),
         useMaterial3: true,
+        textTheme: GoogleFonts.poppinsTextTheme().apply(
+          fontSizeFactor: getFontSize(globalFontSize) / 16,
+        ),
       ),
       initialRoute: '/login',
       routes: {
         '/login': (context) => const LoginPage(),
         '/home': (context) => const HomePage(),
-      },
+      }
     );
   }
 }
-
